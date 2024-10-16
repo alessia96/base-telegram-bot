8ba5ed5f6cb944e81b8e5f49cc2d9fe53b8d9021
alessia96 <alessia.meloni1996@gmail.com>
2024-10-16 14:57:24 +0200

    ci: Add process_message

--- utils.py
+++ utils.py
@@ -14,6 +14,28 @@ MAX_HISTORY = os.environ.get('MAX_HISTORY', 15)
 OPENAI_APIKEY = os.getenv('OPENAI_APIKEY')
 
 
+history = dict()
+
+
+def process_message(text: str, username: str) -> str:
+
+    if username not in history:
+        messages = [{"role": "user", "content": text}]
+        history[username] = messages
+    else:
+        history[username] += [{"role": "user", "content": text}]
+        messages = history[username]
+
+    response = get_openai_response(messages=messages)
+
+    if response != DEFAULT_RESPONSE:
+        history[username].append({"role": "assistant", "content": response})
+
+    if len(history[username]) > MAX_HISTORY:
+        history[username] = history[username][-MAX_HISTORY:]
+
+    return response
+
 
 def get_openai_response(messages: list) -> str:
     client = openai.OpenAI(api_key=OPENAI_APIKEY)
