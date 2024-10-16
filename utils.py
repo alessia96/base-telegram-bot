d4f5f4ba4ef7161cbf3486697cc495e40106873d
alessia96 <alessia.meloni1996@gmail.com>
2024-10-16 14:57:15 +0200

    ci: Add get_openai_response

--- utils.py
+++ utils.py
@@ -14,3 +14,25 @@ MAX_HISTORY = os.environ.get('MAX_HISTORY', 15)
 OPENAI_APIKEY = os.getenv('OPENAI_APIKEY')
 
 
+
+def get_openai_response(messages: list) -> str:
+    client = openai.OpenAI(api_key=OPENAI_APIKEY)
+
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-4-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful friend."
+                }
+            ] + messages
+        )
+    except (Exception,) as e:
+        logger.error(e)
+        return DEFAULT_RESPONSE
+    if len(completion.choices) == 0:
+        return DEFAULT_RESPONSE
+
+    output = completion.choices[0].message
+    return output
